37a83718aefd4d2912aedcbe4502e7dee2801bed
Dominic Faulring <domcodesjs@gmail.com>
2020-09-29 15:30:59 -0700

    commented out smth

--- src/App.js
+++ src/App.js
@@ -3,13 +3,11 @@ import Views from './components/Views';
 import Sidebar from './components/Sidebar';
 import ControlBar from './components/ControlBar';
 
-const App = (props) => {
-  //console.log(props.store.participants);
-  const participants = props.store.participants;
+const App = ({ store }) => {
   return (
-    <main className="session">
+    <main className='session'>
       <Sidebar></Sidebar>
-      <Views participants={participants}></Views>
+      <Views participants={store.participants}></Views>
       <ControlBar></ControlBar>
     </main>
   );
--- src/components/Views.js
+++ src/components/Views.js
@@ -1,29 +1,28 @@
 import React from 'react';
-import './Views.css'; 
+import './Views.css';
 
 const Views = (props) => {
-
-  const hostView = (
-    <div className="owner-view">
-      <h3>Optimus Prime</h3>
-      <img
-        src="https://robohash.org/doloredolorescupiditate.jpg?size=200x200&set=set1"
-        alt="avatar images"
-        className="portraits"
-      />
-    </div>
-  );
+  // const hostView = (
+  //   <div className="owner-view">
+  //     <h3>Optimus Prime</h3>
+  //     <img
+  //       src="https://robohash.org/doloredolorescupiditate.jpg?size=200x200&set=set1"
+  //       alt="avatar images"
+  //       className="portraits"
+  //     />
+  //   </div>
+  // );
   const viewsArray = props.participants.map((index) => {
     return (
       index.onStage && (
-        <div className="host-views" key={index.id}>
+        <div className='host-views' key={index.id}>
           <ul>
             <li>
               <h3>{index.name}</h3>
               <img
-                className="portraits"
+                className='portraits'
                 src={index.avatar}
-                alt="avatar images"
+                alt='avatar images'
               />
             </li>
           </ul>
@@ -32,13 +31,7 @@ const Views = (props) => {
     );
   });
   console.log(viewsArray);
-  return (
-    <div className="views-container">
-      {viewsArray}
-    </div>
-  );
+  return <div className='views-container'>{viewsArray}</div>;
 };
 
 export default Views;
-
-      
